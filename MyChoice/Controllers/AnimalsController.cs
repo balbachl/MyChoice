714c1e0eed29c73a45241f903c584e8e8c470211
Lisa <lisajbalbach@gmail.com>
2020-01-11 22:43:40 -0500

    Add project files.

---
+++ MyChoice/Controllers/AnimalsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyChoice.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        // GET: Animals
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string selCategory)
+        {
+            var endangeredType = HttpUtility.HtmlEncode(selCategory);
+            var viewName = "";
+            switch (endangeredType)
+            {
+                case "Critical":
+                    viewName= "Critical";
+                    break;
+                case "Endangered":
+                    viewName = "Endangered";
+                    break;
+                case "Near Threatened":
+                    viewName = "nearThreatened";
+                    break;
+                case "Vulnerable":
+                    viewName = "Vulnerable";
+                    break;
+                default:
+                    viewName = "LeastThreatened";
+                    break;
+            }
+            return View(viewName);
+        }
+       
+        [HttpPost]
+        public ActionResult AnimalAdoptionForm(string name, string donation)
+        {
+            if (name == "")
+                name = "animal";
+            ViewBag.message = "Thank you for your wonderful donation of " + donation;
+            ViewBag.message += ".  The " + name + " you adopted will greatly benefit from your generousity.";
+            return View("ThankYou");
+        }
+        public ActionResult AnimalAdoptionForm()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyChoice/Models/AnimalModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyChoice.Models
+{
+    public class AnimalModel
+    {
+        public string name { get; set; }
+        //critical, endangered, near threatened, vulnerable, least threatened
+        public string classification { get; set; } = "critical";
+        public double donation { get; set; } = 100.00;
+    }
+}
